ca3ca8cd86b11e27446ea9f7bdec7aa3b9516eb3
yenrudee <65623886+yenrudee@users.noreply.github.com>
2020-05-20 02:24:14 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'SXFbOLl9S0CQhHjE4upaiuAiPwRphKzumvNOIW8F1I9uymeb1GOv4en0DX6ANx00pRXeCxlrwPIsX1USlCd/MyEeBLAizr6ZzqgIUjoJ5yp2QwkmORPlIPHJlFFMVhYXsEQgDjdAUjGLc4/otDq6EwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
